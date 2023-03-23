8170d2b563d8f7f7199bba030e5a5de77fa29fbf
期贤 <qixian.cs@antgroup.com>
2023-03-23 14:12:29 +0800

    优化逻辑

--- src/findAllLess.ts
+++ src/findAllLess.ts
@@ -39,76 +39,154 @@ export const findAllTsxFiles = (dir: string) => {
   });
   return lessFiles;
 };
-const dirPath = '/Users/shuaichen/Documents/github/ant-design-pro/src/';
+// const dirPath = '/Users/shuaichen/Documents/github/ant-design-pro/src/';
 
-findAllLessFiles(dirPath).map((item) => {
-  if (item.endsWith('global.less')) {
-    return;
-  }
-  const content = fs.readFileSync(item, 'utf-8');
-  const ts = lessToCssInJs(
-    content
-      .replaceAll(
-        '.textOverflowMulti();',
-        ` overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      word-break: break-all;`
-      )
-      .replaceAll(
-        '.clearfix();',
-        ` zoom: 1;
-          &::before,
-          &::after {
-            display: table;
-            content: ' ';
-          }
-          &::after {
-            clear: both;
-            height: 0;
-            font-size: 0;
-            visibility: hidden;
-          }`
-      )
-      .replaceAll(
-        '.textOverflow();',
-        ` position: relative;
-      max-height: 4.5em;
-      margin-right: -1em;
-      padding-right: 1em;
-      overflow: hidden;
-      line-height: 1.5em;
-      text-align: justify;
-      &::before {
-        position: absolute;
-        right: 14px;
-        bottom: 0;
-        padding: 0 1px;
-        background: @bg;
-        content: '...';
-      }
-      &::after {
-        position: absolute;
-        right: 14px;
-        width: 1em;
-        height: 1em;
-        margin-top: 0.2em;
-        background: white;
-        content: '';
-      }`
-      )
-  );
-  // console.log(item.replaceAll(dirPath, '') + ' ' + '😁 编译成功');
-  fs.writeFileSync(item.replace('.less', '.style.ts'), ts);
+// findAllLessFiles(dirPath).map((item) => {
+//   if (item.endsWith('global.less')) {
+//     return;
+//   }
+//   const content = fs.readFileSync(item, 'utf-8');
+//   const ts = lessToCssInJs(
+//     content
+//       .replaceAll(
+//         '.textOverflowMulti();',
+//         ` overflow: hidden;
+//       white-space: nowrap;
+//       text-overflow: ellipsis;
+//       word-break: break-all;`
+//       )
+//       .replaceAll(
+//         '.clearfix();',
+//         ` zoom: 1;
+//           &::before,
+//           &::after {
+//             display: table;
+//             content: ' ';
+//           }
+//           &::after {
+//             clear: both;
+//             height: 0;
+//             font-size: 0;
+//             visibility: hidden;
+//           }`
+//       )
+//       .replaceAll(
+//         '.textOverflow();',
+//         ` position: relative;
+//       max-height: 4.5em;
+//       margin-right: -1em;
+//       padding-right: 1em;
+//       overflow: hidden;
+//       line-height: 1.5em;
+//       text-align: justify;
+//       &::before {
+//         position: absolute;
+//         right: 14px;
+//         bottom: 0;
+//         padding: 0 1px;
+//         background: @bg;
+//         content: '...';
+//       }
+//       &::after {
+//         position: absolute;
+//         right: 14px;
+//         width: 1em;
+//         height: 1em;
+//         margin-top: 0.2em;
+//         background: white;
+//         content: '';
+//       }`
+//       )
+//   );
+//   // console.log(item.replaceAll(dirPath, '') + ' ' + '😁 编译成功');
+//   fs.writeFileSync(item.replace('.less', '.style.ts'), ts);
 
-  findAllTsxFiles(path.parse(item).dir).map((jsFileItem) => {
-    const tsxContent = fs.readFileSync(jsFileItem, 'utf-8');
-    if (tsxContent.includes(".less'")) {
-      try {
-        fs.writeFileSync(jsFileItem, stylesImportToCssInJs(tsxContent));
-      } catch (error) {
-        console.log(error, jsFileItem);
-      }
+//   findAllTsxFiles(path.parse(item).dir).map((jsFileItem) => {
+//     const tsxContent = fs.readFileSync(jsFileItem, 'utf-8');
+//     if (tsxContent.includes(".less'")) {
+//       try {
+//         fs.writeFileSync(jsFileItem, stylesImportToCssInJs(tsxContent));
+//       } catch (error) {
+//         console.log(error, jsFileItem);
+//       }
+//     }
+//   });
+// });
+
+/**
+ * 转换 less 和 tsx文件
+ * @param dir 文件夹目录，最好是 src
+ */
+export const transformCssAndTsx = (dir: string) => {
+  findAllLessFiles(dir).map((item) => {
+    if (item.endsWith('global.less')) {
+      return;
     }
+    const content = fs.readFileSync(item, 'utf-8');
+    const ts = lessToCssInJs(
+      content
+        .replaceAll(
+          '.textOverflowMulti();',
+          ` overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        word-break: break-all;`
+        )
+        .replaceAll(
+          '.clearfix();',
+          ` zoom: 1;
+            &::before,
+            &::after {
+              display: table;
+              content: ' ';
+            }
+            &::after {
+              clear: both;
+              height: 0;
+              font-size: 0;
+              visibility: hidden;
+            }`
+        )
+        .replaceAll(
+          '.textOverflow();',
+          ` position: relative;
+        max-height: 4.5em;
+        margin-right: -1em;
+        padding-right: 1em;
+        overflow: hidden;
+        line-height: 1.5em;
+        text-align: justify;
+        &::before {
+          position: absolute;
+          right: 14px;
+          bottom: 0;
+          padding: 0 1px;
+          background: @bg;
+          content: '...';
+        }
+        &::after {
+          position: absolute;
+          right: 14px;
+          width: 1em;
+          height: 1em;
+          margin-top: 0.2em;
+          background: white;
+          content: '';
+        }`
+        )
+    );
+    // console.log(item.replaceAll(dirPath, '') + ' ' + '😁 编译成功');
+    fs.writeFileSync(item.replace('.less', '.style.ts'), ts);
+
+    findAllTsxFiles(path.parse(item).dir).map((jsFileItem) => {
+      const tsxContent = fs.readFileSync(jsFileItem, 'utf-8');
+      if (tsxContent.includes(".less'")) {
+        try {
+          fs.writeFileSync(jsFileItem, stylesImportToCssInJs(tsxContent));
+        } catch (error) {
+          console.log(error, jsFileItem);
+        }
+      }
+    });
   });
-});
+};
